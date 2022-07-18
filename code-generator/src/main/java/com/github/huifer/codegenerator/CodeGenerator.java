e601963aae50f1dbd38acb0ff50572b72fb27a06
Zen Huifer <huifer97@163.com>
2022-07-18 21:16:49 +0800

    feat: 代码生成器

---
+++ code-generator/src/main/java/com/github/huifer/codegenerator/CodeGenerator.java
@@ -0,0 +1,72 @@
+package com.github.huifer.codegenerator;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.fill.Column;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class CodeGenerator {
+    public static final String MYSQL_INFO = "mysql.properties";
+
+    static String url;
+    static String username;
+    static String password;
+    static String author;
+    static String packageName;
+    static String table;
+    static String out_put_file;
+
+    static {
+        InputStream in = CodeGenerator.class.getClassLoader().getResourceAsStream(MYSQL_INFO);
+        Properties properties = new Properties();
+        try {
+            properties.load(in);
+            url = properties.getProperty("url");
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
+            author = properties.getProperty("author");
+            packageName = properties.getProperty("package");
+            table = properties.getProperty("table");
+            table = properties.getProperty("table");
+            out_put_file = properties.getProperty("out_put_file");
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create(url, username, password)
+                .globalConfig((scanner, builder) -> builder.author(author).fileOverride())
+                .packageConfig((scanner, builder) -> builder.parent(packageName)
+                        .pathInfo(Collections.singletonMap(OutputFile.xml, out_put_file)))
+
+                .strategyConfig((scanner, builder) -> builder.addInclude(getTables(table))
+                        .controllerBuilder().enableRestStyle().enableHyphenStyle()
+                        .entityBuilder().enableLombok().addTableFills(
+                                new Column("create_time", FieldFill.INSERT),
+                                new Column("create_user_id", FieldFill.INSERT),
+                                new Column("update_time", FieldFill.INSERT_UPDATE),
+                                new Column("update_user_id", FieldFill.INSERT_UPDATE)
+
+                        ).build())
+                .execute();
+
+
+    }
+
+    protected static List<String> getTables(String tables) {
+        return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
+    }
+}
