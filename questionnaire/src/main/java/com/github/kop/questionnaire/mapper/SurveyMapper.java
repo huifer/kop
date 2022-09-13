f318bdb94106a039dbb54832457d98ce75436de3
hiABandon <hiabandon@outlook.com>
2022-09-13 14:18:30 +0800

    fix: questionnaire缺少Mapper注解导致项目无法启动

--- questionnaire/src/main/java/com/github/kop/questionnaire/mapper/SurveyMapper.java
+++ questionnaire/src/main/java/com/github/kop/questionnaire/mapper/SurveyMapper.java
@@ -2,6 +2,7 @@ package com.github.kop.questionnaire.mapper;
 
 import com.github.kop.questionnaire.entity.Survey;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author Zen Huifer
  * @since 2022-09-07
  */
+@Mapper
 public interface SurveyMapper extends BaseMapper<Survey> {
 
 }
