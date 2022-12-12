f139858508ee9f242448fde67f6d14ab36065cf3
Zen Huifer <huifer97@163.com>
2022-12-12 09:56:51 +0800

    gorm

--- rbac/go/handler/company.go
+++ rbac/go/handler/company.go
@@ -1,9 +1,26 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"rbac/db"
+	"rbac/entity"
+	"rbac/request"
+	"rbac/utils"
+)
 
 // 创建
 func CreateCompany(c *gin.Context) {
+	req := request.CompanyCreateRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		company := entity.Company{Name: req.Name}
+
+		db.GetDB().Save(company)
+
+		utils.Ok(c, true)
+
+	}
+	utils.Error(c, -1, "创建失败")
 
 }
 
---
+++ rbac/go/request/companyCreate.go
@@ -0,0 +1,5 @@
+package request
+
+type CompanyCreateRequest struct {
+	Name string `json:"name"`
+}
